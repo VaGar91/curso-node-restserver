48e70bde5d4e2922223c732182b55b940fbd692f
VicAb Garcia <victor.ag71@gmail.com>
2023-02-28 16:30:27 -0600

    Backend listo

---
+++ controllers/usuarios.js
@@ -0,0 +1,50 @@
+const { response } = require('express');
+
+const usuariosGet = (req, res = response) => {
+
+    const {q, nombre, apikey} = req.query;
+
+    res.json({
+        ok: true,
+        msg: 'get API - controlador',
+        q,
+        nombre,
+        apikey
+    });
+}
+
+const usuariosPost = (req, res = response) => {
+    const {nombre, edad} = req.body;
+
+    res.json({
+        ok: true,
+        msg: 'post API - controlador',
+        nombre,
+        edad
+    });
+}
+
+const usuariosPut = (req, res = response) => {
+    const id = req.params.id;
+
+    res.json({
+        ok: true,
+        msg: 'put API - controlador',
+        id
+    });
+}
+
+const usuariosDelete = (req, res = response) => {
+    res.json({
+        ok: true,
+        msg: 'delete API - controlador'
+    });
+}
+
+
+module.exports = {
+    usuariosGet,
+    usuariosPost,
+    usuariosPut,
+    usuariosDelete
+}
